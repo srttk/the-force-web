5520d5ccbec7d64b20b7d08b197be8376a48fef4
Sarath <sarath.tk@icloud.com>
2020-04-02 21:37:16 +0530

    Single page animation imporvements :car:

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,11 @@ const Layout = ({ children }) => {
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -15 }}
-        transition={{ duration: 0.4 }}
+        transition={{
+          duration: 0.3,
+          staggerChildren: 0.5,
+          when: "beforeChildren"
+        }}
       >
         {children}
       </motion.div>
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,17 +2,26 @@ import React from "react";
 import { RichText } from "prismic-reactjs";
 import { getPostBySlug } from "../../libs/api";
 import Layout from "../../components/Layout";
+import { motion } from "framer-motion";
 
 const Post = ({ slug, post }) => {
   return (
     <Layout>
       <div className="px-2 py-2 mt-4 shadow">
-        <h1 className="text-2xl text-indigo-600 font-thin hover:text-indigo-900">
+        <motion.h1
+          initial={{ y: -20 }}
+          animate={{ y: 0 }}
+          className="text-2xl text-indigo-600 font-thin hover:text-indigo-900"
+        >
           {RichText.asText(post.data.title)}
-        </h1>
-        <p className="text-gray-800 font-light text-lg">
+        </motion.h1>
+        <motion.p
+          initial={{ y: 20 }}
+          animate={{ y: 0 }}
+          className="text-gray-800 font-light text-lg"
+        >
           {RichText.asText(post.data.content)}
-        </p>
+        </motion.p>
       </div>
     </Layout>
   );
